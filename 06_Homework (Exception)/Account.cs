ddc4f00a50d84367eda3cbff6d58c645b1bd9239
Mykola Martsiv <mykola.martsiv@gmail.com>
2023-04-03 13:00:26 +0300

    Completed homework #6 task 1

---
+++ 06_Homework (Exception)/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Homework__Exception_
+{
+    internal class Account
+    {
+        private string email;
+        public string Email 
+        { 
+            get { return email; }
+            set
+            {
+                if ((4 < value.Length) && (value.Length < 50) &&
+                    (value.Where(x => (char)x == '@').ToArray().Length == 1) &&
+                    (value.Where(x => char.IsLetter(x) || char.IsDigit(x) || (x == '_') || (x == '@')).ToArray().Length == value.Length))
+                {
+                    Console.WriteLine("Seccess email");
+                    email = value;
+                }
+                else
+                    throw new ArgumentException("Not correct format email");
+            }
+        }
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set 
+            {
+                if ((6 < value.Length) &&
+                    (0 < value.Where(x => char.IsLetter(x)).ToArray().Length) &&
+                    (0 < value.Where(x => char.IsDigit(x)).ToArray().Length))
+                {
+                    Console.WriteLine("Seccess email");
+                    password = value;
+                }
+                else
+                    throw new ArgumentException("Not correct format password");
+            }
+        }
+        public Account(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ 06_Homework (Exception)/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _06_Homework__Exception_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            string password = Console.ReadLine();
+            Account acc = new Account(email, password);
+        }
+    }
+}
